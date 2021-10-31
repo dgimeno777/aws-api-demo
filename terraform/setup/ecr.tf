8f8bff4055ee52988137713782272b5708a04a74
Danny <danny@Dannys-MacBook-Air.local>
2021-10-31 16:39:41 -0400

    add docker support to repo

---
+++ terraform/setup/ecr.tf
@@ -0,0 +1,12 @@
+resource "aws_ecr_repository" "aws_api_repo" {
+  name = "aws-api-repo-${local.resource_name_suffix}"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+  tags = {
+    (var.tag_project_key): local.tag_project_value
+  }
+}
---
+++ terraform/setup/variables.tf
@@ -0,0 +1,10 @@
+locals {
+  resource_name_suffix = terraform.workspace
+  tag_project_value = "aws-api-demo"
+}
+
+variable "tag_project_key" {
+  type = string
+  description = "The project tag key"
+  default = "Project"
+}
