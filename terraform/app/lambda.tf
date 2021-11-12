ba8a2a9d4d5966556f93790653d89b51121b1bfe
Danny <danny@Dannys-MacBook-Air.local>
2021-11-11 19:35:42 -0500

    working aws lambda

--- terraform/app/lambda.tf
+++ terraform/app/lambda.tf
@@ -18,7 +18,4 @@ resource "aws_lambda_function" "aws_api_demo_get_random_color" {
   role          = aws_iam_role.aws_api_demo_get_random_color.arn
   package_type = "Image"
   image_uri = "${data.aws_ecr_repository.aws-api-demo-api.repository_url}:latest"
-  image_config {
-    command = ["poetry", "run", "python", "-m", "api.lambda_handler.get_random_color.get_random_color_lambda_handler"]
-  }
 }
